5d6c3659a18a00058fb4aeab8bb6441fbe2405b4
Warrick Smith <warrick.smith@neo.nz>
2023-12-22 11:36:19 +1300

    feat: add base game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,102 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
+
+const Board = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 100px);
+  grid-gap: 10px;
+`
+
+const Cell = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: lightgray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+  cursor: pointer;
+`
+
+const Display = styled.div`
+  margin-top: 20px;
+  font-size: 18px;
+`
+
+const App: React.FC = () => {
+  const [board, setBoard] = useState(Array(9).fill(''))
+  const [isXNext, setIsXNext] = useState(true)
+  const winner = calculateWinner(board)
+
+  const handleClick = (index: number) => {
+    if (board[index] || winner) return
+
+    const newBoard = [...board]
+    newBoard[index] = isXNext ? 'X' : 'O'
+
+    setBoard(newBoard)
+    setIsXNext(!isXNext)
+  }
+
+  const renderCell = (index: number) => {
+    return (
+      <Cell
+        key={index}
+        onClick={() => handleClick(index)}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        {board[index]}
+      </Cell>
+    )
+  }
+
+  const getStatus = () => {
+    if (winner) {
+      return `Winner: ${winner}`
+    } else {
+      return `Next player: ${isXNext ? 'X' : 'O'}`
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container>
+      <Board>
+        {Array.from({ length: 9 }, (_, index) => renderCell(index))}
+      </Board>
+      <Display>{getStatus()}</Display>
+    </Container>
   )
+}
+
+const calculateWinner = (squares: string[]): string | null => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  for (const [a, b, c] of lines) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+
+  return null
 }
 
 export default App
