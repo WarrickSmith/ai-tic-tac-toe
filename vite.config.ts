6a16731cd00bec4d3fb994b1ad01d1f9b68726af
Warrick Smith <warrick.smith@neo.nz>
2024-03-28 23:34:01 +1300

    chore:update the base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/tic-tac-toe/",
+  base: "/",
   plugins: [react()],
   preview: {
     port: 3000,
